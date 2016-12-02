c96149fa2a0f11b9c2edef26b41966bc4aacd6e2
Geno Guerrero <geno.guerrero@gmail.com>
2016-12-02 10:33:31 -0800

    finished MVP

--- js/app.js
+++ js/app.js
@@ -8,13 +8,9 @@ function Project(data) {
   this.description = data.description;
 };
 Project.prototype.toHtml = function() {
-  var $newProject = $('.template').clone();
-  $newProject.find('.pname').html(this.pname);
-  $newProject.find('a').attr('href', this.purl);
-  $newProject.find('.pdate').html(this.pdate);
-  $newProject.find('.description').html(this.description);
-  $newProject.removeClass('template');
-  return $newProject;
+  var $newProject = $('#project-template').html();
+  var template = Handlebars.compile($newProject);
+  return template(this);
 };
 devProjects.forEach(function(projectObj) {
   projects.push(new Project(projectObj));
@@ -23,12 +19,12 @@ projects.forEach(function(projectObj) {
   $('#projects').append(projectObj.toHtml());
 });
 
-$('section').not('#home').hide();
-
-$('nav').on('click', '.tab', function(event){
-  var $activeDiv = $(this).attr('href');
-  event.preventDefault();
-  $activeDiv.show(800);
-  // $activeDiv.hide(1600);
-  console.log('I was clicked');
-});
+// $('section').not('#home').hide();
+//
+// $('nav').on('click', '.tab', function(event){
+//   var $activeDiv = $(this).attr('href');
+//   event.preventDefault();
+//   $activeDiv.show(800);
+//   // $activeDiv.hide(1600);
+//   console.log('I was clicked');
+// });
